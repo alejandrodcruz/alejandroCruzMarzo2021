32c03c1e3862b5a207b5506243ac07fd449a23a3
Alejandro Cruz <alejandrodavidcruz@gmail.com>
2021-03-03 21:14:47 -0300

    Test OK

--- test/ar/edu/unlam/pb2/ordenes/LineaOrdenTest.java
+++ test/ar/edu/unlam/pb2/ordenes/LineaOrdenTest.java
@@ -1,10 +1,21 @@
 package ar.edu.unlam.pb2.ordenes;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class LineaOrdenTest {
 	@Test
-	public void queSePuedeCalcularElTotalPorDetalle() {		
+	public void queSePuedeCalcularElTotalPorDetalle() {	
+		Producto prod1 = new Producto(1, "Arroz", 60.0);
+		Producto prod2 = new Producto(2, "Azucar", 30.0);
+		LineaOrden linea1 = new LineaOrden(prod1, 2);
+		LineaOrden linea2 = new LineaOrden(prod2, 2);
+		Orden orden = new Orden();
+		orden.agregarLinea(linea1);
+		orden.agregarLinea(linea2);
+		assertEquals(orden.obtenerPrecioTotal(), 10.0, 0.0);
+
 	}
 	
 }
